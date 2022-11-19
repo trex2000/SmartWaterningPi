99ab890ebb2eea9469d2e3afd8ac8d6a867f9c73
Szidonia16 <szidii.1998@gmail.com>
2022-11-19 15:34:30 +0200

    SSW-2, SSW-7: the led and button test melted togheter

---
+++ IO_LedButtonTest.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)  # using the GPIO pin numbers instead of 'standard' pin numbers
+GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)  # 26 and 19 pin is set as an output
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)  # the two pins initial value is set to low (off)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # GPIO  16, 20 is set as an input
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # the input pin initial value is HIGH, we use pull up resistor
+# pull up resistor: connect between a pin and VCC, with an open switch connected between pin and GND
+# pull up resistor keeps the input HIGH
+
+if GPIO.input(16) == 0:  # when the pin is false (LOW state) the button is pushed
+    GPIO.output(26, GPIO.HIGH)  # turns on the two pins, the pins are made to provide power of 3.3volts
+else:
+    GPIO.output(26, GPIO.LOW)  # turns off the two pins,the pins are no longer supplying any power
+if GPIO.input(20) == 0:
+    GPIO.output(19, GPIO.HIGH)
+else:
+    GPIO.output(19, GPIO.LOW)
