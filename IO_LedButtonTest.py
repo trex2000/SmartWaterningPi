4546a9be7b8474a05ad66c60eb90104d768894cd
Szidonia16 <szidii.1998@gmail.com>
2022-11-20 11:14:13 +0200

    SSWS-2, SSWS-7: corrected

--- IO_LedButtonTest.py
+++ IO_LedButtonTest.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)  # using the GPIO pin numbers instead of 'standard' pin numbers
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)  # 26 and 19 pin is set as an output
 GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)  # the two pins initial value is set to low (off)
@@ -8,11 +9,12 @@ GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # the input pin initial value
 # pull up resistor: connect between a pin and VCC, with an open switch connected between pin and GND
 # pull up resistor keeps the input HIGH
 
-if GPIO.input(16) == 0:  # when the pin is false (LOW state) the button is pushed
-    GPIO.output(26, GPIO.HIGH)  # turns on an LED
-else:
-    GPIO.output(26, GPIO.LOW)  # turns off the LED
-if GPIO.input(20) == 0:
-    GPIO.output(19, GPIO.HIGH)
-else:
-    GPIO.output(19, GPIO.LOW)
+while True:
+    if GPIO.input(16) == 0:  # when the pin is false (LOW state) the button is pushed
+        GPIO.output(26, GPIO.HIGH)  # turns on an LED
+    else:
+        GPIO.output(26, GPIO.LOW)  # turns off the LED
+    if GPIO.input(20) == 0:
+        GPIO.output(19, GPIO.HIGH)
+    else:
+        GPIO.output(19, GPIO.LOW)
