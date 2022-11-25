0bc6306db3c5876c2c9e25620f52e2c079539c3b
Szidonia16 <szidii.1998@gmail.com>
2022-11-25 20:07:04 +0200

    SSWS-30: one push button debounced

--- IO_PushButtonDebounced.py
+++ IO_PushButtonDebounced.py
@@ -11,25 +11,22 @@ GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # the input pin initial value
 # pull up resistor: connect between a pin and VCC, with an open switch connected between pin and GND
 # pull up resistor keeps the input HIGH
 
-led1_state = False  # means that the LED is off
-led2_state = False
+debounce_counter = 500
+led1_state = False  # LED1 is off
 
 while True:
-    if GPIO.input(16) == 0:  # the button was pushed, pin is connected to the GND (0/LOW/False)
-        if led1_state == False:
-            GPIO.output(26, GPIO.HIGH)
-            led1_state = True  # the LED is on
-            sleep(.5)
+    if GPIO.input(16) == 0:
+        print('Button 1 was pressed')  # button is pressed
+        if debounce_counter == 0 and led1_state == False:  # if the button is pressed for 5s and
+            GPIO.output(26, GPIO.HIGH)                    # the LED is off than it is going to turn on
+            led1_state = True
+        elif debounce_counter == 0 and led1_state == True:  # if the LED is already on and
+            GPIO.output(26, GPIO.LOW)                      # the button is pressed for 5s the LED is going to
+            led1_state = False                              # turn off
         else:
-            GPIO.output(26, GPIO.LOW)
-            led1_state = False
-            sleep(.5)
-    if GPIO.input(20) == 0:
-        if led2_state == False:
-            GPIO.output(19, GPIO.HIGH)
-            led2_state = True
-            sleep(.5)
-        else:
-            GPIO.output(19, GPIO.LOW)
-            led2_state = False
-            sleep(.5)
+            debounce_counter = debounce_counter - 1
+            print(debounce_counter)
+            print(led1_state)
+    else:
+        debounce_counter = 500  # if you depress the button the counter will be again 500
+    sleep(.01)
