439fa83a729818d025bf274e294ef59d06298230
Szidonia16 <szidii.1998@gmail.com>
2022-11-26 19:21:04 +0200

    SSWS-30: I tried ?!

--- IO_PushButtonDebounced.py
+++ IO_PushButtonDebounced.py
@@ -13,20 +13,50 @@ GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # the input pin initial value
 
 debounce_counter = 500
 led1_state = False  # LED1 is off
+led2_state = False  # LED2 is off
 
-while True:
-    if GPIO.input(16) == 0:
-        print('Button 1 was pressed')  # button is pressed
-        if debounce_counter == 0 and led1_state == False:  # if the button is pressed for 5s and
-            GPIO.output(26, GPIO.HIGH)                    # the LED is off than it is going to turn on
-            led1_state = True
-        elif debounce_counter == 0 and led1_state == True:  # if the LED is already on and
-            GPIO.output(26, GPIO.LOW)                      # the button is pressed for 5s the LED is going to
-            led1_state = False                              # turn off
+
+def control_first_led():
+    global debounce_counter
+    global led1_state
+    while True:
+        if GPIO.input(16) == 0:
+            print('Button 1 was pressed')  # button is pressed
+            if debounce_counter == 0 and led1_state == False:  # if the button is pressed for 5s and
+                GPIO.output(26, GPIO.HIGH)  # the LED is off than it is going to turn on
+                led1_state = True
+            elif debounce_counter == 0 and led1_state == True:  # if the LED is already on and
+                GPIO.output(26, GPIO.LOW)  # the button is pressed for 5s the LED is going to
+                led1_state = False  # turn off
+            else:
+                debounce_counter = debounce_counter - 1
+                print(debounce_counter)
+                print(led1_state)
         else:
-            debounce_counter = debounce_counter - 1
-            print(debounce_counter)
-            print(led1_state)
-    else:
-        debounce_counter = 500  # if you depress the button the counter will be again 500
-    sleep(.01)
+            debounce_counter = 500  # if you depress the button the counter will be again 500
+        sleep(.01)
+
+
+def control_second_led():
+    global debounce_counter
+    global led2_state
+    while True:
+        if GPIO.input(20) == 0:
+            print('Button 2 was pressed')
+            if debounce_counter == 0 and led2_state == False:
+                GPIO.output(19, GPIO.HIGH)
+                led2_state = True
+            elif debounce_counter == 0 and led2_state == True:
+                GPIO.output(19, GPIO.LOW)
+                led2_state = False
+            else:
+                debounce_counter = debounce_counter - 1
+                print(debounce_counter)
+                print(led2_state)
+        else:
+            debounce_counter = 500
+        sleep(.01)
+
+
+control_first_led()
+control_second_led()
