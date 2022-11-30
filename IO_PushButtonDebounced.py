d0a2d4b686632063415b3bdcd607c755ea844850
Szidonia16 <szidii.1998@gmail.com>
2022-11-30 15:43:40 +0200

    SSWS-30: implemented generic counter functions

--- IO_PushButtonDebounced.py
+++ IO_PushButtonDebounced.py
@@ -2,11 +2,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 T_DEBOUNCE_INIT_VALUE = 500  # debounce counter initial value
+T_SLEEP = 0.01  # sleep time
+CNT_ELAPSED_VAL = 0  # counter was running and reached 0
+CNT_STOPPED_VAL = 0xFFFF  # counter was stopped and is not running
 PUSH_BUT1 = 16  # push button 1 pin number
 PUSH_BUT2 = 20  # push button 2 pin number
 LED1 = 26  # LED 1 pin number
 LED2 = 19  # LED 2 pin number
-T_SLEEP = 0.01  # sleep time
 
 
 def setup():
@@ -22,6 +24,27 @@ def setup():
 # pull up resistor: connect between a pin and VCC, with an open switch connected between pin and GND
 # pull up resistor keeps the input HIGH
 
+def is_counter_running(counter):
+    if (counter != CNT_ELAPSED_VAL) and (counter != CNT_STOPPED_VAL):
+        return True
+    else:
+        return False
+
+
+def is_counter_elapsed(counter):
+    return counter == CNT_ELAPSED_VAL
+
+
+def start_counter(counter, init_value):
+    counter = init_value
+    return counter
+
+
+def stop_counter(counter):
+    counter = CNT_STOPPED_VAL
+    return counter
+
+
 setup()  # initialising the input and output ports
 
 debounce_counter1 = T_DEBOUNCE_INIT_VALUE
@@ -31,17 +54,18 @@ led2_state = False  # LED2 is off
 
 while True:
     if GPIO.input(PUSH_BUT1) == 0:  # button is pressed
-        if debounce_counter1 == 0:  # if the button is pressed for 5s and
-            print('counter elapsed')
-            debounce_counter1 = T_DEBOUNCE_INIT_VALUE
+        if is_counter_running(debounce_counter1):  # if the button is pressed for 5s and
+            debounce_counter1 = debounce_counter1 - 1
+        elif is_counter_elapsed(debounce_counter1):
+            debounce_counter1 = stop_counter(debounce_counter1)
+            print('counter 5s elapsed')
             if led1_state == False:
                 GPIO.output(LED1, GPIO.HIGH)
                 led1_state = True
             elif led1_state == True:
                 GPIO.output(LED1, GPIO.LOW)
                 led1_state = False
-        else:
-            debounce_counter1 = debounce_counter1 - 1
     else:
-        debounce_counter1 = T_DEBOUNCE_INIT_VALUE  # if you depress the button the counter will be again 500
+        debounce_counter1 = start_counter(debounce_counter1,
+                                          T_DEBOUNCE_INIT_VALUE)  # if you depress the button the counter will be again 500
     sleep(T_SLEEP)
