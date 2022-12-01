b904e24b4b5f3bd2028ea198c3818c49f60e0b9a
Szidonia16 <szidii.1998@gmail.com>
2022-12-01 15:15:55 +0200

    SSWS-3: organised and commented

--- IO_OLEDTest.py
+++ IO_OLEDTest.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 OLED_ADDRESS = 0x3c  # the I2C address of the OLED display
 WIDTH = 128  # width size of the display
 HEIGHT = 64  # height size of the display
+PADDING = 5  # 5 bit space created inside the border, this is also the top of the left corner
 
 i2c = busio.I2C(board.SCL, board.SDA)  # create the I2C instance
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=OLED_ADDRESS)
@@ -21,16 +22,16 @@ oled.show()
 image = Image.new('1', (WIDTH, HEIGHT))
 draw = ImageDraw.Draw(image)
 
-padding = 2
-shape_width = 20
-top = padding
-bottom = HEIGHT - padding
-x = padding
-
-# load default font
+# load the default font
 font = ImageFont.load_default()
-draw.text((x, top), 'Hello', font=font, fill=255)
-draw.text((x, top + 10), 'Szidi', font=font, fill=255)
+
+# drawing on the blank image
+draw.text((PADDING, PADDING), 'Hello', font=font, fill=255)
+# first param: top left corner where the text going to start
+# second param: the text
+# third param: the font you are using
+# fourth param: color to use for the text
+draw.text((PADDING, PADDING + 10), 'Szidi', font=font, fill=255)
 
 # display image
 oled.image(image)
