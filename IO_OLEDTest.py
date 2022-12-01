6da039a171f3caa72c66fedb168600d024a7eb13
Szidonia16 <szidii.1998@gmail.com>
2022-12-01 13:56:21 +0200

    SSWS-3: display Hello Szidi

--- IO_OLEDTest.py
+++ IO_OLEDTest.py
@@ -1,8 +1,37 @@
-from machine import Pin, I2c
-import ssd1306
+import board  # import board pins
+import busio
+import adafruit_ssd1306  # import the SSD1306 module.
+from PIL import Image, ImageDraw, ImageFont
 
-i2c = I2C(sda=Pin(2), scl=Pin(3))
-display = ssd1306.SSD1306_I2C(128, 64, i2c)
+OLED_ADDRESS = 0x3c  # the I2C address of the OLED display
+WIDTH = 128  # width size of the display
+HEIGHT = 64  # height size of the display
 
-display.text('Hello!', 0, 0, 1)
-display.show()
+i2c = busio.I2C(board.SCL, board.SDA)  # create the I2C instance
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=OLED_ADDRESS)
+# initializing the OLED display
+# first two parameters: the pixel width, height
+# last parameter: the I2C address of the device
+
+# clearing the display
+oled.fill(0)
+oled.show()
+
+# creating a blank image for drawing
+image = Image.new('1', (WIDTH, HEIGHT))
+draw = ImageDraw.Draw(image)
+
+padding = 2
+shape_width = 20
+top = padding
+bottom = HEIGHT - padding
+x = padding
+
+# load default font
+font = ImageFont.load_default()
+draw.text((x, top), 'Hello', font=font, fill=255)
+draw.text((x, top + 10), 'Szidi', font=font, fill=255)
+
+# display image
+oled.image(image)
+oled.show()
