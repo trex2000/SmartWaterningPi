f0e46ef3298df31da1187f9794152bffb1101fbe
Szidonia16 <szidii.1998@gmail.com>
2023-01-14 13:04:01 +0200

    SSWS-33: first sample of inserting soil moisture records into PMA's database (Error: Not enough paramaeters for the SQL statement)

--- IO_SoilMoistureSensor.py
+++ IO_SoilMoistureSensor.py
@@ -1,26 +1,48 @@
 import time
+import datetime
+import adafruit_ads1x15.ads1015 as ADS  # importing the analog digital converter named: ADS
 import board
 import busio  # importing the I2C bus
-import adafruit_ads1x15.ads1015 as ADS  # importing the analog digital converter named: ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-
-
-def values():
-    x = 100 * 8000 / int(chan.value)
-    percentage = int(x)
-    if chan.value > 18400:
-        print(f'The soil is {percentage}% dry.')
-    elif 8000 < chan.value < 18400:
-        print(f'The soil is {percentage}% moist.')
-    elif chan.value < 8000:
-        print(f'The soil is {percentage}% wet. ')
-
+from getpass import getpass
+from mysql.connector import connect, Error
 
 i2c = busio.I2C(board.SCL, board.SDA)  # creating the I2C bus
 ads = ADS.ADS1015(i2c)  # creating the ADC object using the I2C bus
 chan = AnalogIn(ads, ADS.P0)  # creating single-ended input on channel 0
 
 
-while True:
-    values()
-    time.sleep(2)
+def insert_soil_moisture_table(device, date, raw_value, percentage, classification):
+    try:
+        with connect(
+                host="localhost",
+                user=input("Enter username: "),
+                password=getpass("Enter password: "),
+                database='smartwatering'
+        ) as connection:
+            soil_moisture_query = "INSERT INTO SOIL_MOISTURE (DEVICE, DATE, RAW_VALUE, PERCENTAGE, CLASSIFICATION)"\
+                                  "VALUES (%s, %s, %s, %s, %s)"
+            soil_moisture_records = (device, date, raw_value, percentage, classification)
+            with connection.cursor() as cursor:
+                cursor.executemany(soil_moisture_query, soil_moisture_records)
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
+
+
+try:
+    while True:
+        device = 'ADS 1X15'
+        now = datetime.datetime.now()
+        date = now.strftime('%Y-%m-%d %H:%M:%S')
+        raw_value = chan.value
+        percentage = 100 * 8000 / int(chan.value)
+        classification = 'class'
+        insert_soil_moisture_table((device,), date, raw_value, percentage, (classification,))
+        time.sleep(5)
+except(IOError, TypeError) as e:
+    print(e)
+except KeyboardInterrupt:
+    print('stopping')
