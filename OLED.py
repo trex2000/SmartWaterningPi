c4d92d60328738829ea43cd8904c7da26e72236b
Szidonia16 <szidii.1998@gmail.com>
2023-01-21 16:03:58 +0200

    SSWS-35: defined a function for each menu page

--- OLED.py
+++ OLED.py
@@ -11,34 +11,34 @@ PADDING = 1  # 5 bit space created inside the border, this is also the top of th
 
 i2c = busio.I2C(board.SCL, board.SDA)  # create the I2C instance
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=OLED_ADDRESS)
+
+
 # initializing the OLED display
 # first two parameters: the pixel width, height
 # last parameter: the I2C address of the device
 
-# clearing the display
-oled.fill(0)
-oled.show()
-time.sleep(5)
-
-# creating a blank image for drawing
-image = Image.new('1', (WIDTH, HEIGHT))
-draw = ImageDraw.Draw(image)
-
-# load the default font
-font = ImageFont.load_default()
-
 
 def highlight_DHT22_Logging():
     global PADDING
-    global draw
-    global font
+    global oled
 
-    ''' Drawing on the blank image:
-    first parameter: top left corner where the text going to start
-    second parameter: the text
-    third parameter: the font you are using
-    fourth parameter: color to use for the text'''
+    # clearing the display
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
 
+    # creating a blank image for drawing
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    # load the default font
+    font = ImageFont.load_default()
+
+    # Drawing on the blank image:
+    # first parameter: top left corner where the text going to start
+    # second parameter: the text
+    # third parameter: the font you are using
+    # fourth parameter: color to use for the text
     draw.text((PADDING, PADDING), '~SMART WATERING MENU~', font=font, fill=255)
     draw.text((PADDING, PADDING + 20), '> 1. DHT22 Logging <', font=font, fill=255)
     draw.text((PADDING, PADDING + 30), '2. Moisture Logging', font=font, fill=255)
@@ -51,8 +51,16 @@ def highlight_DHT22_Logging():
 
 def highlight_Moisture_Logging():
     global PADDING
-    global draw
-    global font
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
 
     draw.text((PADDING, PADDING), '~SMART WATERING MENU~', font=font, fill=255)
     draw.text((PADDING, PADDING + 20), '1. DHT22 Logging', font=font, fill=255)
@@ -66,8 +74,16 @@ def highlight_Moisture_Logging():
 
 def highlight_Automatic_Watering():
     global PADDING
-    global draw
-    global font
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
 
     draw.text((PADDING, PADDING), '~SMART WATERING MENU~', font=font, fill=255)
     draw.text((PADDING, PADDING + 20), '1. DHT22 Logging', font=font, fill=255)
@@ -81,8 +97,16 @@ def highlight_Automatic_Watering():
 
 def highlight_Manual_Watering():
     global PADDING
-    global draw
-    global font
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
 
     draw.text((PADDING, PADDING), '~SMART WATERING MENU~', font=font, fill=255)
     draw.text((PADDING, PADDING + 20), '1. DHT22 Logging', font=font, fill=255)
@@ -96,8 +120,16 @@ def highlight_Manual_Watering():
 
 def highlight_DHT22_opt1():
     global PADDING
-    global draw
-    global font
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
 
     draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
     draw.text((PADDING, PADDING + 20), '> 1. Logging <', font=font, fill=255)
@@ -109,8 +141,504 @@ def highlight_DHT22_opt1():
     oled.show()
 
 
-while True:
-    if input('write "a": ') == 'a':
-        highlight_DHT22_Logging()
-    else:
-        highlight_DHT22_opt1()
+def highlight_DHT22_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. Logging', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. Current Temperature <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current Humidity', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. Logging', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. Current Temperature', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Current Humidity <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_opt4():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. Logging', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. Current Temperature', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current Humidity', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '> 4. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_opt1():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '> 1. Logging <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. Current Value', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. Logging', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. Current Value <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. Logging', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. Current Value', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Automatic_Watering_opt1():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Automatic Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '> 1. ON <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Automatic_Watering_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Automatic Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON ', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. OFF <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Automatic_Watering_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Automatic Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON ', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Current State <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Automatic_Watering_opt4():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Automatic Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '> 4. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Manual_Watering_opt1():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Manual Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '> 1. ON <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Manual_Watering_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Manual Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. OFF <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Manual_Watering_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Manual Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Current State <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Manual_Watering_opt4():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Manual Watering~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '> 4. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_Logging_opt1():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '> 1. ON <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_Logging_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. OFF <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_Logging_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Current State <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_DHT22_Logging_opt4():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~DHT22 Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '> 4. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_Logging_opt1():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '> 1. ON <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_Logging_opt2():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '> 2. OFF <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_Logging_opt3():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '> 3. Current State <', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '4. Back', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+
+def highlight_Moisture_Logging_opt4():
+    global PADDING
+    global oled
+
+    oled.fill(0)
+    oled.show()
+    time.sleep(5)
+
+    image = Image.new('1', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.load_default()
+
+    draw.text((PADDING, PADDING), '~Moisture Logging~', font=font, fill=255)
+    draw.text((PADDING, PADDING + 20), '1. ON', font=font, fill=255)
+    draw.text((PADDING, PADDING + 30), '2. OFF', font=font, fill=255)
+    draw.text((PADDING, PADDING + 40), '3. Current State', font=font, fill=255)
+    draw.text((PADDING, PADDING + 50), '> 4. Back <', font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
