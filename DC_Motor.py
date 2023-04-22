44ac9f407cb281035edf3599c39af3590cf012ba
Szidonia16 <szidii.1998@gmail.com>
2023-04-22 20:06:39 +0300

    SSWS-5: commented and organized

--- DC_Motor.py
+++ DC_Motor.py
@@ -1,31 +1,58 @@
+"""!Turns on the DC motor.
+"""
+
+#Imports
 import RPi.GPIO as GPIO
 from time import sleep
 
-IN1 = 17  # assigning the pin numbers
+#Global Constants
+## Input one that controls the H-bridge.
+IN1 = 17
+## Input two that controls the H-bridge
 IN2 = 27
+## Input for motor A that controls the PWM.
 ENA = 13
+## Setting the frequency.
+pwm = GPIO.PWM(ENA, 1000)
+## Input the answer.
+answer = input('To run the motor forward press "r".\n To tun the motor backward press "b".')
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)  # IN1 and IN2 controls the direction in which the motor will spin
-GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)  # their initial values are assigned as low
-GPIO.setup(ENA, GPIO.OUT)  # controls the speed of the motor
-pwm = GPIO.PWM(ENA, 1000)  # the frequency is set to 100Hz
-pwm.start(50)  # and the duty cycle is set to 50 which will make the motor run at 50% speed
+#Functions
+def DCmotor_setup():
+    """!Setup the library to use board numbering.
+    
+    Uses the GPIO pin numbers instead of 'standard' pin numbers.
+    Initializez the IN1 and IN2, which controls the spining direction, their initial value is set low.
+    Initializez the ENA output, which controls the speed.
+    """
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)   
+    GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW) 
+    GPIO.setup(ENA, GPIO.OUT) 
 
 
 def run_forward():
+    """!The motor spins forward.
+    """
+
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
 
 
 def run_backward():
+    """!The motor spins backward.
+    """
+
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
 
 
-answer = input('To run the motor forward press "r".\n To tun the motor backward press "b".')
+DCmotor_setup()
+# The duty cycle is set to 50 which will make the motor run at 50% speed.
+pwm.start(50)
 
 while True:
     print(answer)
@@ -34,4 +61,3 @@ while True:
     else:
         run_backward()
     sleep(10)
-
