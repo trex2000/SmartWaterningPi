64b7c869c159e4b33a8f953cfde8c28abcc20f89
Szidonia16 <szidii.1998@gmail.com>
2023-09-23 15:27:38 +0300

    SSWS-54: Added configuration of task manager on Szidi's laptop

--- Arduino/SmartWatering/src/main.cpp
+++ Arduino/SmartWatering/src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
+#include <TaskScheduler.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+// Scheduler
+Scheduler ts;
+
+/*Task periods*/
+#define PERIOD1 10
+#define PERIOD2 20
+
+/*The Callback function for the 10 ms task*/
+void Tasks10ms(){
+}
+
+/*The Callback function for the 20 ms task*/
+void Tasks20ms(){
+}
+
+/*The 10 and the 20 ms task*/
+Task Task10ms ( PERIOD1 * TASK_MILLISECOND, TASK_FOREVER , &Tasks10ms, &ts, true );
+Task Task20ms ( PERIOD2* TASK_MILLISECOND, TASK_FOREVER , &Tasks20ms, &ts, true );
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  /*Start the task scheduler*/
+  ts.execute();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
